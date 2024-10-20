b6fcf1ebfab39e714faa0b330fd8b3fd889debfb
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-10-20 21:09:08 +1000

    📦 Aspire upgrade (#77)
    * Update Aspire packages
    
    * Remove Aspire from GitHub
    
    * Configured container dependencies
    
    * Update service defaults
    
    * Update sql resource name

--- tools/MigrationService/Worker.cs
+++ tools/MigrationService/Worker.cs
@@ -18,8 +18,9 @@ public class Worker(
 
         try
         {
-            logger.LogInformation("Waiting for SQL Server to be ready");
-            await Task.Delay(10_000, cancellationToken);
+            // TODO: Check that this is no longer needed in Aspire .NET 9
+            // logger.LogInformation("Waiting for SQL Server to be ready");
+            // await Task.Delay(10_000, cancellationToken);
 
             var sw = Stopwatch.StartNew();
             using var scope = serviceProvider.CreateScope();
